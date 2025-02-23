f8e44766646d82a6b5f902c42c21516fe11f3125
enesterov <nesterov@satissoft.ru>
2025-02-23 23:00:47 +0300

    tests

--- hiscript/src/test/java/TestComplex.java
+++ hiscript/src/test/java/TestComplex.java
@@ -22,9 +22,20 @@ public class TestComplex extends HiTest {
 		//		assertFailCompile("switch(\"\"){case String s: break;}"); // default required
 		//		assertFailCompile("switch(\"\"){case Object o: break; case String s: break;}"); // Object before String
 		//		assertFailCompile("switch(\"\"){case Integer i: break; case String s: break;}"); // not all cases
+
+		// assertSuccess("class C{static int x = 1; static C get(){return null;}} assert C.get().x == 1;"); // compiler change C.get() to C
+		// assertSuccess("float f1 = 0.0f; float f2 = -0.0f; assert f1 != f2;");
+		// assertSuccess("enum E{a,b}; E[] values = E.values(); assert values.length == 2; assert values[0] == E.a; assert values[1] == E.b;");
+		// assertSuccess("enum E{a,b}; assert E.valueOf(\"a\") == E.a;  assert E.valueOf(\"b\") == E.b;  assert E.valueOf(\"c\") == null;");
 	}
 
 	@Test
 	public void testNew() throws HiScriptParseException, TokenizerException, HiScriptValidationException {
+		// float
+		assertSuccess("assert 0.0f == -0.0f;");
+
+		// methods overriding
+		assertSuccess("class A{int m(Object o){return 1;} int m(Number o){return 2;} int m(Integer o){return 3;}} assert new A().m(null) == 3;");
+		assertSuccess("class A{ int m(Integer o){return 3;} int m(Object o){return 1;} int m(Number o){return 2;}} assert new A().m(null) == 3;");
 	}
 }
